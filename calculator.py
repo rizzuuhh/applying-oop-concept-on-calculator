69ce2d737fcb70db062835132fbe55f9cf0ecc59
rizza <denostarizza43@gmail.com>
2023-05-23 17:40:39 +1200

    Pseudocode

---
+++ calculator.py
@@ -0,0 +1,22 @@
+# Constructor: Initialize the title 
+    # Print the title of the calculator 
+    # Print the menu options for the calculator
+    # Get user input with the provided message as prompt
+    # Perform the specified operation on the given numbers
+    # Addition
+    # Subtraction
+    # Multiplication
+    # Division
+    # Print the result of the calculation
+    # Check if the result is a float (for division)
+    # Run the calculator
+    # Print the menu options
+    # Get the operation from the user
+    # Get the first number from the user
+    # Get the second number from the user
+    # Perform the operation
+    # Print the result
+    # Ask if the user wants to repeat
+    # Print the thank you message
+# Create an instance of the Calculator class
+# Run the calculator
